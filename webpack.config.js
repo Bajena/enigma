bc9c3b4ef97b21b680415c3cc99cec7b64986f99
Jan Bajena <bajena3@gmail.com>
2021-01-09 22:01:30 +0100

    Watch and generate source maps

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   entry: './src/index.js',
   mode: 'production',
+  devtool: 'inline-source-map',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
